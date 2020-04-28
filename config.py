a09c6d912d14f56959a14a81a6902581c8168df6
സൽമാൻ <61979182+salmanpdr@users.noreply.github.com>
2020-04-28 12:21:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TOKEN", None)
+    TOKEN = os.environ.get("TOKEN", None)
     # required for running on Heroku
     URL = os.environ.get("URL", "")
     PORT = int(os.environ.get("PORT", 5000))
